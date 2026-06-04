1590017e74da1417796c6d0b20b7f3da7249a946
karnish0812-ship-it <karnish0812@gmail.com>
2026-06-04 15:40:12 +0530

    javascript completed

---
+++ javascript/script.js
@@ -0,0 +1,99 @@
+// var a=10;
+// let b=20;
+// const c=30;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// var num=10;
+// var str="karnish";
+// var undefinedVar;
+// var Null=null;
+// var boolean=true;
+ 
+// console.log( typeof num);
+// console.log( typeof str);
+// console.log( typeof undefinedVar);
+// console.log( typeof Null);
+// console.log( typeof boolean);
+
+// var bigint=123n;
+// var symbol=Symbol("karnish");
+
+// console.log( bigint);
+// console.log( symbol)
+// ;
+// var arr =[1,2,3,4,5];
+// console.log(typeof  arr, arr);
+
+// var obj={
+//     name:"karnish",
+//     dept:["cse","it"],
+// }
+// console.log( typeof obj, obj);
+
+
+// var a=10;
+// var b=2;
+// console.log(a+b);   
+// console.log(a-b);
+// console.log(a*b);
+// console.log (a%b);
+// console.log(a/b);
+
+// logical operators
+
+// var a=true;
+// var b=false;
+// console.log(a && b);
+// console.log(a || b);
+// console.log(!a);    
+
+// relational operators
+
+// var a=10;
+// var b=20;
+// console.log(a>b);
+// console.log(a<b);
+// console.log(a>=b);
+// console.log(a<=b);
+// console.log(a==b);
+// console.log(a!=b);
+// console.log(a===b);
+// console.log(a!==b);  
+
+
+// assignment operator
+
+// var a=10;
+// var b=20;
+ 
+// a+=b;
+// console.log(a);
+// a-=b;
+// console.log(a);
+// a*=b;
+// console.log(a);
+// a/=b;
+// console.log(a);
+// a%=b;
+// console.log(a);
+// a**=b;
+// console.log(a);
+
+// var a=10;
+//  console.log(a++);
+//  console.log (++a);
+
+//  var a=10;
+//  console.log(a--);
+//  console.log(--a);
+
+
+var a=1;
+var b=1;
+var c=0;
+
+var result = a++ + --b + ++c - ++a + ++b + c++ + b++ + a;
+console.log (result)
+
